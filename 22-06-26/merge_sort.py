c6d4c5ccf62fae29453a051285e0c2024d813d82
arefin-ahmed <arefin-ahmed@users.noreply.github.com>
2026-06-27 13:43:37 +0600

    update merge sort- problem in print

--- 22-06-26/merge_sort.py
+++ 22-06-26/merge_sort.py
@@ -1,36 +1,41 @@
-def merge(A, lb, mid, ub): 
+def merge(A, lb, mid, ub):
     i = lb
     j = mid + 1
     k = lb
-    b = [0]*len(A)
+    b = [0] * len(A)
 
-    while i<=mid and j<=ub:
+    while i <= mid and j <= ub:
         if A[i] <= A[j]:
             b[k] = A[i]
-            i+=1
-            k+=1
+            i += 1
         else:
             b[k] = A[j]
-            j+=1
-            k+=1
-        if i>mid:
-            while j<=ub:
-                k+=1
-                j+=1
-        else:
-            while i<=mid:
-                b[k]= A[i]
-                k+=1
-                i+=1
-    for k in range (lb, ub):
-        A[k] = b[k]
+            j += 1
+        k += 1
+
+    while i <= mid:
+        b[k] = A[i]
+        i += 1
+        k += 1
+
+    while j <= ub:
+        b[k] = A[j]
+        j += 1
+        k += 1
+
+    for idx in range(lb, ub + 1):
+        A[idx] = b[idx]
+
 
 def merge_sort(A, lb, ub):
-    if lb<ub:
-        mid = (lb+ub)//2
+    if lb < ub:
+        mid = (lb + ub) // 2
         merge_sort(A, lb, mid)
-        merge_sort(A, mid+1, ub)
+        merge_sort(A, mid + 1, ub)
         merge(A, lb, mid, ub)
 
-A = list(map(int, input("Enter numbers: ").split()))
-print("Sorted Array: ", merge_sort(A, 0, len(A)-1)) 
+
+if __name__ == "__main__":
+    A = list(map(int, input("Enter numbers: ").split()))
+    merge_sort(A, 0, len(A) - 1)
+    print("Sorted Array:", A)
