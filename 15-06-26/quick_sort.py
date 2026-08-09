b163b4aff45f6473d28b6d824b4d3e1851d9d303
arefin-ahmed <arefin-ahmed@users.noreply.github.com>
2026-08-10 05:10:00 +0600

    fix error for logic and print

--- 15-06-26/quick_sort.py
+++ 15-06-26/quick_sort.py
@@ -9,17 +9,19 @@ def partition(A, lb, ub):
     i = lb + 1
     j = ub
 
-    while i < j:
-        while A[i] <= A[pivot]:
-            i =+ 1
-        while A[j] > A[pivot]:
-            j =-1
+    while i <= j:
+        while i <= ub and A[i] <= pivot:
+            i += 1
+        while j <= ub and A[j] > pivot:
+            j -= 1
         if i < j:
             A[i], A[j] = A[j], A[i]
         else:
-            A[pivot], A[j] = A[j], A[pivot]
+            A[lb], A[j] = A[j], A[lb]
 
     return j
 
-j = list(map(int, input("Enter numbers: ").split()))
-print("Sorted Array: ", quick_sort(j, 0, len(j)-1))
+A = list(map(int, input("Enter numbers: ").split()))
+quick_sort(A, 0, len(A)-1)
+
+print("Sorted Array: ", A)
